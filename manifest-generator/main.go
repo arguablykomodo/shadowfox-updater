a5ffbb6ae916b553e85c7f0f2b37d95fd0fa4724
Sr Komodo <komodosr@gmail.com>
2018-09-25 14:46:27 -0300

    apparently we do need the manifest version, fixes #20

---
+++ manifest-generator/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	findStr := []byte(`    name="SrKomodo.Software.shadowfoxUpdater"
+`)
+
+	os.Remove("manifest.xml")
+
+	manifest, err := ioutil.ReadFile("_manifest.xml")
+	if err != nil {
+		if !os.IsNotExist(err) {
+			panic(err)
+		}
+	}
+
+	err = ioutil.WriteFile(
+		"manifest.xml",
+		bytes.Replace(
+			manifest,
+			findStr,
+			append(
+				findStr,
+				[]byte("    version=\""+strings.TrimPrefix(os.Getenv("V"), "v")+"\"\n")...,
+			),
+			-1,
+		),
+		0644,
+	)
+	if err != nil {
+		panic(err)
+	}
+}
