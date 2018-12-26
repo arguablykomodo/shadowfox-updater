7ea1c8bd822e984186b2ebd2da8896d22dcf1841
Sr Komodo <komodosr@gmail.com>
2018-12-26 19:41:50 -0300

    concatenate paths instead of breaking on first path found

--- profiles.go
+++ profiles.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"path/filepath"
 
 	"github.com/go-ini/ini"
@@ -23,42 +22,31 @@ func getProfilePaths() ([]string, []string) {
 	// iniPaths stores all profiles.ini files we have to check
 	iniPaths := []string{}
 
-	// Find current directory
-	cwd, err := os.Executable()
-	checkErr("Couldn't find current directory", err)
+	// Get the home directory
+	homedir, err := homedir.Dir()
+	checkErr("Couldn't find home directory", err)
 
-	// Check if profiles.ini exists in the cwd
-	exists, _, err := pathExists(filepath.Join(filepath.Dir(cwd), "profiles.ini"))
-	checkErr("Couldn't check if profiles.ini exists in current directory", err)
-	if exists { // If it does we just stop here
-		iniPaths = []string{filepath.Join(filepath.Dir(cwd), "profiles.ini")}
-	} else { // If not we will do some more stuff
-		// Get the home directory
-		homedir, err := homedir.Dir()
-		checkErr("Couldn't find home directory", err)
+	// Possible places where we should check for profiles.ini
+	possible := []string{
+		"./profiles.ini",
+		homedir + "\\AppData\\Roaming\\Mozilla\\Firefox\\profiles.ini",
+		homedir + "/Library/Application Support/Firefox/profiles.ini",
+		homedir + "/.mozilla/firefox/profiles.ini",
+		homedir + "/.mozilla/firefox-trunk/profiles.ini",
+	}
 
-		// Possible places where we should check for profiles.ini
-		possible := []string{
-			homedir + "\\AppData\\Roaming\\Mozilla\\Firefox\\profiles.ini",
-			homedir + "/Library/Application Support/Firefox/profiles.ini",
-			homedir + "/.mozilla/firefox/profiles.ini",
-			homedir + "/.mozilla/firefox-trunk/profiles.ini",
+	// Check if profiles.ini exists on each possible path and add them to the list
+	for _, p := range possible {
+		exists, _, err := pathExists(p)
+		checkErr("Couldn't check if "+p+" exists", err)
+		if exists {
+			iniPaths = append(iniPaths, p)
 		}
+	}
 
-		// Check if profiles.ini exists on each possible path and add them to the list
-		for _, p := range possible {
-			exists, _, err := pathExists(p)
-			checkErr("Couldn't check if "+p+" exists", err)
-			if exists {
-				iniPaths = append(iniPaths, p)
-				break
-			}
-		}
-
-		// If we didnt find anything then we just give up
-		if len(iniPaths) == 0 {
-			return nil, nil
-		}
+	// If we didnt find anything then we just give up
+	if len(iniPaths) == 0 {
+		return nil, nil
 	}
 
 	var paths []string
