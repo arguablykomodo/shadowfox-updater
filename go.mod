1a833ccc9f08e39e6e4018d6742eea56874eadbb
Sr Komodo <komodosr@gmail.com>
2019-05-18 17:20:40 -0300

    switch to ui

--- go.mod
+++ go.mod
@@ -1,15 +1,7 @@
 module github.com/SrKomodo/shadowfox-updater
 
 require (
-	github.com/akavel/rsrc v0.0.0-20170831122431-f6a15ece2cfd // indirect
-	github.com/gdamore/tcell v1.1.1
-	github.com/go-ini/ini v1.41.0
-	github.com/gopherjs/gopherjs v0.0.0-20181103185306-d547d1d9531e // indirect
-	github.com/josephspurrier/goversioninfo v0.0.0-20190122232203-2e127a11672c // indirect
-	github.com/jtolds/gls v4.2.1+incompatible // indirect
-	github.com/mitchellh/go-homedir v1.0.0
-	github.com/rivo/tview v0.0.0-20190122192331-1ac6fbc0c239
-	github.com/smartystreets/assertions v0.0.0-20190116191733-b6c0e53d7304 // indirect
-	github.com/smartystreets/goconvey v0.0.0-20181108003508-044398e4856c // indirect
-	gopkg.in/ini.v1 v1.41.0 // indirect
+	github.com/andlabs/ui v0.0.0-20180902183112-867a9e5a498d
+	github.com/go-ini/ini v1.42.0
+	github.com/mitchellh/go-homedir v1.1.0
 )
--- ui.go
+++ ui.go
@@ -1,151 +1,77 @@
 package main
 
 import (
-	"github.com/gdamore/tcell"
-
-	"github.com/rivo/tview"
+	"github.com/andlabs/ui"
+	_ "github.com/andlabs/ui/winmanifest"
 )
 
-var infoText *tview.TextView
-
-func notifyErr(message string, err error) {
-	infoText.SetTextColor(tcell.ColorWhite)
-	infoText.SetBackgroundColor(tcell.ColorRed)
-	if message != "" {
-		infoText.SetText(message + ":\n" + err.Error())
-	} else {
-		infoText.SetText(err.Error())
-	}
-}
-
 func createUI() error {
-	app := tview.NewApplication()
-	paths, names := getProfilePaths()
-	profileIndex := 0
+	err := ui.Main(func() {
+		paths, names := getProfilePaths()
 
-	infoText = tview.NewTextView()
-	infoText.SetText("(Press TAB for selecting, ENTER for clicking)")
-	infoText.SetTextAlign(tview.AlignCenter)
+		infoText := ui.NewLabel("")
 
-	// Create buttons
-	profileSelect := tview.NewDropDown().SetLabel("Profile to use: ").SetOptions(names, func(text string, index int) {
-		profileIndex = index
-	})
-
-	uuidCheckBox := tview.NewCheckbox().SetLabel("Auto-Generate UUIDs: ").SetChecked(false)
-
-	themeCheckBox := tview.NewCheckbox().SetLabel("Set Firefox dark theme: ").SetChecked(false)
-
-	installButton := tview.NewButton("Install/Update ShadowFox").SetSelectedFunc(func() {
-		message, err := install(paths[profileIndex], uuidCheckBox.IsChecked(), themeCheckBox.IsChecked())
-		if err != nil {
-			notifyErr(message, err)
-		} else {
-			infoText.SetText("ShadowFox was successfully installed!")
+		dropdown := ui.NewCombobox()
+		for _, name := range names {
+			dropdown.Append(name)
 		}
-	})
+		dropdown.SetSelected(0)
 
-	uninstallButton := tview.NewButton("Uninstall ShadowFox").SetSelectedFunc(func() {
-		message, err := uninstall(paths[profileIndex])
-		if err != nil {
-			notifyErr(message, err)
-		} else {
-			infoText.SetText("ShadowFox was successfully uninstalled!")
-		}
-	})
+		dropdownForm := ui.NewForm()
+		dropdownForm.SetPadded(true)
+		dropdownForm.Append("Profile to use:", dropdown, false)
 
-	exitButton := tview.NewButton("Exit").SetSelectedFunc(func() {
-		app.Stop()
-	})
+		installButton := ui.NewButton("Install/Update ShadowFox")
+		uninstallButton := ui.NewButton("Uninstall ShadowFox")
+		exitButton := ui.NewButton("Exit")
 
-	// Setup input callbacks so that TAB switches between buttons
-	app.SetInputCapture(func(e *tcell.EventKey) *tcell.EventKey {
-		if e.Key() == tcell.KeyTAB {
-			if e.Modifiers()&tcell.ModShift == tcell.ModShift {
-				switch {
-				case exitButton.HasFocus():
-					app.SetFocus(uninstallButton)
-				case uninstallButton.HasFocus():
-					app.SetFocus(installButton)
-				case installButton.HasFocus():
-					app.SetFocus(themeCheckBox)
-				case themeCheckBox.HasFocus():
-					app.SetFocus(uuidCheckBox)
-				case uuidCheckBox.HasFocus():
-					app.SetFocus(profileSelect)
-				case profileSelect.HasFocus():
-					app.SetFocus(exitButton)
-				}
+		buttons := ui.NewHorizontalBox()
+		buttons.SetPadded(true)
+		buttons.Append(installButton, true)
+		buttons.Append(uninstallButton, true)
+		buttons.Append(exitButton, true)
+
+		uuidCheckBox := ui.NewCheckbox("Auto-Generate UUIDs")
+		themeCheckBox := ui.NewCheckbox("Set Firefox dark theme")
+
+		installButton.OnClicked(func(b *ui.Button) {
+			message, err := install(paths[dropdown.Selected()], uuidCheckBox.Checked(), themeCheckBox.Checked())
+			if err != nil {
+				infoText.SetText(message + ":\n" + err.Error())
 			} else {
-				switch {
-				case profileSelect.HasFocus():
-					app.SetFocus(uuidCheckBox)
-				case uuidCheckBox.HasFocus():
-					app.SetFocus(themeCheckBox)
-				case themeCheckBox.HasFocus():
-					app.SetFocus(installButton)
-				case installButton.HasFocus():
-					app.SetFocus(uninstallButton)
-				case uninstallButton.HasFocus():
-					app.SetFocus(exitButton)
-				case exitButton.HasFocus():
-					app.SetFocus(profileSelect)
-				}
+				infoText.SetText("ShadowFox was successfully installed!")
 			}
-		}
-		return e
+		})
+
+		uninstallButton.OnClicked(func(b *ui.Button) {
+			message, err := uninstall(paths[dropdown.Selected()])
+			if err != nil {
+				infoText.SetText(message + ":\n" + err.Error())
+			} else {
+				infoText.SetText("ShadowFox was successfully uninstalled!")
+			}
+		})
+
+		exitButton.OnClicked(func(b *ui.Button) {
+			ui.Quit()
+		})
+
+		box := ui.NewVerticalBox()
+		box.SetPadded(true)
+		box.Append(infoText, false)
+		box.Append(dropdownForm, false)
+		box.Append(uuidCheckBox, false)
+		box.Append(themeCheckBox, false)
+		box.Append(buttons, false)
+
+		window := ui.NewWindow("Shadowfox Updater "+version, 1, 1, false)
+		window.SetMargined(true)
+		window.SetChild(box)
+		window.OnClosing(func(*ui.Window) bool {
+			ui.Quit()
+			return true
+		})
+		window.Show()
 	})
-
-	flex := tview.NewFlex().SetDirection(tview.FlexRow).
-		AddItem(tview.NewFlex().
-			AddItem(nil, 0, 1, false).
-			AddItem(profileSelect, 0, 10, true).
-			AddItem(nil, 0, 1, false), 1, 0, true,
-		).
-		AddItem(nil, 1, 0, false).
-		AddItem(tview.NewFlex().
-			AddItem(nil, 0, 1, false).
-			AddItem(uuidCheckBox, 0, 10, true).
-			AddItem(nil, 0, 1, false), 1, 0, true,
-		).
-		AddItem(nil, 1, 0, false).
-		AddItem(tview.NewFlex().
-			AddItem(nil, 0, 1, false).
-			AddItem(themeCheckBox, 0, 10, true).
-			AddItem(nil, 0, 1, false), 1, 0, true,
-		).
-		AddItem(nil, 1, 0, false).
-		AddItem(infoText, 5, 0, false).
-		AddItem(nil, 1, 0, false).
-		AddItem(tview.NewFlex().
-			AddItem(installButton, 0, 1, true).
-			AddItem(nil, 1, 0, false).
-			AddItem(uninstallButton, 0, 1, true).
-			AddItem(nil, 1, 0, false).
-			AddItem(exitButton, 0, 1, true), 3, 0, true,
-		)
-	flex.SetBorder(true).
-		SetTitle("ShadowFox updater "+version).
-		SetBorderPadding(1, 1, 1, 1).
-		SetBackgroundColor(tcell.ColorBlack)
-
-	if paths == nil {
-		text := tview.NewTextView().SetText(
-			"ShadowFox couldn't automatically find 'profiles.ini'. Please follow these steps:\n\n" +
-				"1. Close the program                                       \n" +
-				"2. Move the program to the folder 'profiles.ini' is located\n" +
-				"3. Run the program                                         ",
-		).SetTextAlign(tview.AlignCenter)
-
-		text.SetBorder(true).SetTitle("ShadowFox updater "+version).SetBorderPadding(1, 1, 1, 1)
-
-		app.SetRoot(text, true)
-	} else {
-		app.SetRoot(flex, true).SetFocus(profileSelect)
-	}
-
-	if err := app.Run(); err != nil {
-		return err
-	}
-	return nil
+	return err
 }
