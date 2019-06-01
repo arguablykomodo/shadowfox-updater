66a0d2ba62d5c8f14eeb4e657270ca0b36898548
Sr Komodo <komodosr@gmail.com>
2019-06-01 00:30:32 -0300

    switch to batch script for compiling

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+OSLIST=(windows darwin linux)
+ARCHLIST=(386 amd64)
+
+OSNAMES=(windows mac linux)
+ARCHNAMES=(x32 x64)
+
+for I in ${!OSLIST[*]}; do
+  for J in ${!ARCHLIST[*]}; do
+    export GOOS=${OSLIST[$I]}
+    export GOARCH=${ARCHLIST[$J]}
+    OUT=dist/shadowfox_${OSNAMES[$I]}_${ARCHNAMES[$J]}
+    if [ $GOOS = windows ]; then
+      OUT=${OUT}.exe
+    fi
+    
+    go build -o $OUT
+  done
+done
